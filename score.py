a72b77433e544b5f320c246484f6082e5927891b
Tugrul Guner <47668043+tugrulguner@users.noreply.github.com>
2022-02-11 01:59:55 +0000

    Add files via upload

---
+++ score.py
@@ -0,0 +1,21 @@
+import json
+import numpy as np
+import sklearn
+import joblib
+import os
+import pandas as pd
+
+def init():
+    
+    global model
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'),'model.pkl')
+    model = joblib.load(model_path)
+
+def run(input_data):
+    
+    data = pd.read_json(input_data, orient='columns')
+    print(data)
+    # make prediction
+
+    out = model.predict(data)
+    return json.dumps({'Prediction': out.tolist()})
